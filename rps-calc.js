f40878ea7b83b6c70410193c490303ffdcd5f673
gKyle1109 <kyleholmes1119@gmail.com>
2023-08-31 21:30:44 -0700

    displayed outputs of game/rounds in html

--- rps-calc.js
+++ rps-calc.js
@@ -2,8 +2,10 @@ const rps = ["rock", "paper", "scissors"];
 const computerChoice = () => rps[Math.floor(Math.random() * rps.length)]; 
 let playerScore = 0;
 let computerrScore = 0;
+let message = "";
 
 // add dom manipulation to allow player to choice option 
+
 let playerChoice = "";
 const playRock = document.getElementById("Rock");
 const playPaper = document.getElementById("Paper");
@@ -22,28 +24,50 @@ playScissors.addEventListener("click", () => {
 });
 function playRound () {
     const computer = computerChoice();
-    console.log(playerScore)
     if (computer == playerChoice) {
-        return "tie game";
+        message = "Tie Game Play Again"
     } else if (computer == "rock" && playerChoice == "scissors") {
+        message = "You Lose";
         computerrScore += 1;
-        return "you lose";
     } else if (computer == "rock" && playerChoice == "paper") {
+        message = "You Win";
         playerScore += 1;
-        return "you win";
     } else if (computer == "paper" && playerChoice == "scissors") {
+        message = "You Win"
         playerScore += 1;
-        return "you win";
     } else if (computer == "paper" && playerChoice == "rock") {
+        message = "You Lose";
         computerrScore += 1;
-        return "you lose";
     } else if (computer == "scissors" && playerChoice == "paper") {
+        message = "You Lose";
         computerrScore += 1;
-        return "you lose";
     } else if (computer == "scissors" && playerChoice == "rock") {
+        message = "You Win";
         playerScore += 1;
-        return "you win";
     } 
+    results()
 }
 
+//display inputs and outputs of game
 
+const playerPoints = document.getElementById("player-score");
+const oppnetPoints = document.getElementById("computer-score");
+const text = document.getElementById("message");
+function results() {
+    playerPoints.textContent = playerScore;
+    oppnetPoints.textContent =  computerrScore;
+    text.textContent = message;
+    if (playerScore == 5) {
+        playerPoints.textContent = 0;
+        oppnetPoints.textContent = 0;
+        alert("Congrats You Win The Score Was " + playerScore + " To " + computerrScore);
+        playerScore = 0;
+        computerrScore = 0;
+    } else if (computerrScore == 5) {
+        playerPoints.textContent = 0;
+        oppnetPoints.textContent = 0;
+        alert("Sorry You Lose The Score Was " + playerScore + " To " + computerrScore);
+        playerScore = 0;
+        computerrScore = 0;
+    }
+}
